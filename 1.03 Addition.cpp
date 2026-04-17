68c230584406cf22e9bf358def415d2fb84fa839
s4intpaul <160616332+s4intpaul@users.noreply.github.com>
2026-04-17 22:59:07 +1000

    Add files via upload

---
+++ 1.03 Addition.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	double x;
+	cin >> x;
+
+	double y;
+	cin >> y;
+
+	double sum = x + y;
+
+	cout << "\The sum of " << x << " and " << y << " is "
+		<< sum << endl;
+
+
+}
